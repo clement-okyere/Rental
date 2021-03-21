36ff7304ece52ba37e53d1b8567e4dc31c7160d6
okyere <clement.okyere@gmail.com>
2021-03-21 09:23:25 +0000

    added no console to prettier config

--- startup/db.js
+++ startup/db.js
@@ -3,7 +3,7 @@ const winston = require("winston");
 const config = require("config");
 
 module.exports = function () {
-  const db = config.get("db");
+  const db = config.get("db")
   mongoose
     .connect(config.get("db"), { useNewUrlParser: true })
     .then(() => winston.info(`Connected to ${db}....`))
